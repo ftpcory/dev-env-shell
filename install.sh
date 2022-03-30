d414d67282592b19f43ecf8e29f8e8353600d0c4
Cory Collier <cory.collier@waitrapp.com>
2022-03-30 11:37:30 -0400

    more installation fixes

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ mkdir -p "${HOME}/.docker/cli-plugins"
 mkdir -p "${HOME}/.vim"
 mkdir -p "${HOME}/bin"
 
+[[ -f main.zip ]] && rm -f main.zip*
+
 wget "https://github.com/ftpcory/dev-env-shell/archive/refs/heads/main.zip"
 unzip -o main.zip
 cd dev-env-shell-main
@@ -22,8 +24,8 @@ cd dev-env-shell-main
 cp src/.bashrc "${HOME}"
 cp src/.bash_aliases "${HOME}"
 cp src/.vimrc "${HOME}"
-cp -r src/.vim/ "${HOME}/.vim"
-cp -r src/bin/ "${HOME}/bin"
+cp -r src/.vim/* "${HOME}/.vim"
+cp -r src/bin/* "${HOME}/bin"
 
 chmod +x "${HOME}/bin/"*
 chmod +x "${HOME}/.docker/cli-plugins/"*
